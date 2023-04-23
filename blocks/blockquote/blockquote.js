d753f3d996415a19c1906a194171f9457758a26d
Justin Conabree <jconabree@absolunet.com>
2023-04-23 18:20:18 -0400

    block quote component

---
+++ blocks/blockquote/blockquote.js
@@ -0,0 +1,14 @@
+export default function decorate(block) {
+  const quoteDiv = block.querySelector(':scope > div > div');
+  const blockquote = document.createElement('blockquote');
+  blockquote.innerHTML = quoteDiv.innerHTML;
+
+  quoteDiv.replaceWith(blockquote);
+
+  const authorDiv = block.querySelector('div:nth-child(2)');
+  if (authorDiv) {
+    const p = document.createElement('p');
+    p.innerHTML = `<strong><em>${authorDiv.innerHTML}</em></strong>`;
+    authorDiv.replaceWith(p);
+  }
+}
